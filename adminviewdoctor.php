c2c661a04a8079bf31b89aa023a8bff484f09dd7
Reinhard <reinhardanday@gmail.com>
2022-11-11 10:55:18 +0300

    allows admin to access doctor records

---
+++ adminviewdoctor.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="include/styles1.css">
+    <title>PatientProfile</title>
+</head>
+<body>
+
+    <section class="docpage">
+            <h1>Add Doctor </h1>
+           
+        
+        <div class="hospservices">
+        
+
+            <div class="hosp2">
+                <form method="post">
+                        <div class="profile">
+                            <input type="text" placeholder= "Enter Doctor's Name" name= "Doc_Name">
+                            <input type="text" placeholder= "Enter Doctor's ID" name= "Doc_Id">
+                            <input type="text" placeholder= "Department" name= "Department_Id">
+                            <input type="text" placeholder= "Hospital" name= "Hospital_Id">
+                            
+                        </div>
+                    </form>
+            </div>
+               
+            
+        </div>
+    </section>
+    
+
+    
+</body>
+</html>
